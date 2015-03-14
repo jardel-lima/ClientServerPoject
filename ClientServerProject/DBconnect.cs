a9185aaa81b74f4006246857ab922061ccc8d21b
faelpkp <rhafael13@gmail.com>
2015-03-13 19:45:07 -0700

    connect

---
+++ ClientServerProject/DBconnect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerProject
+{
+    class DBconnect
+    {
+        public string host, uid, database, password;
+
+        public override string ToString()
+        {
+            string connectionString;
+
+            connectionString = "SERVER=" + host + ";DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            return connectionString;
+        }
+    }
+}
